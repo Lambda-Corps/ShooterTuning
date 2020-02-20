16df2f9985e893645d490935b053ed69257bbb7b
Scott <scott@pug>
2020-02-19 21:08:28 -0500

    First tuning.

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,65 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+  TalonSRX m_top, m_bottom, m_conveyor;
+  /**
+   * Creates a new ExampleSubsystem.
+   */
+  public Shooter() {
+    m_top = new TalonSRX(8);
+    m_bottom = new TalonSRX(9);
+    m_conveyor = new TalonSRX(2);
+
+    // set talons to factory deafault
+    m_top.configFactoryDefault();
+    m_bottom.configFactoryDefault();
+    m_conveyor.configFactoryDefault();
+
+    // config top motor
+    m_top.setInverted(false);
+    m_top.setSensorPhase(false);
+    m_top.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+
+    // config bottom motor
+    m_bottom.setInverted(false);
+    m_bottom.follow(m_top);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void configureVelocityPID(double kp, double ki, double kd, double kf) {
+    m_top.config_kI(0, kp);
+    m_top.config_kI(0, ki);
+    m_top.config_kD(0, kd);
+    m_top.config_kF(0, kf);
+  }
+
+  public void velocityPID(double m_setpoint, double m_tolerance) {
+    m_top.set(ControlMode.Velocity, m_setpoint);
+  }
+
+  public void stopMotors() {
+    m_top.set(ControlMode.PercentOutput, 0);
+    m_conveyor.set(ControlMode.PercentOutput, 0);
+  }
+
+  public void startConveyor(double speed) {
+    m_conveyor.set(ControlMode.PercentOutput, speed);
+  }
+}
